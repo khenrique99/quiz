07eeb4b529872761ece04bff340b1e6125d638b2
kelvinhenri99 <kelvin5henri@gmail.com>
2022-09-22 16:40:35 -0300

    Atualizando gerais :)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call( class:usuarioSeeder::class);
+        $this->call( class:PerguntasSeeder::class);
     }
 }
---
+++ database/seeders/PerguntasSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PerguntasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('grupos')->insert([
+            'id'        => '1',
+            'grupo'     => 'Área da Saúde',
+            'image'     => 'saude.png',
+        ]);
+        DB::table('grupos')->insert([
+            'id'        => '2',
+            'grupo'     => 'Construção Civil',
+            'image'     => 'construcao.png',
+        ]);
+        DB::table('grupos')->insert([
+            'id'        => '3',
+            'grupo'     => 'Inteligência Artificial',
+            'image'     => 'ia.png',
+        ]);
+        DB::table('grupos')->insert([
+            'id'        => '4',
+            'grupo'     => 'Corpo de Bombeiro',
+            'image'     => 'bombeiro.png',
+        ]);
+        DB::table('grupos')->insert([
+            'id'        => '5',
+            'grupo'     => 'Policial',
+            'image'     => 'policial.png',
+        ]);
+    }
+}
